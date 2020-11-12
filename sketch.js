8cee539bb4aaf2a3995c0c2630516cd736d87e27
nayanajanu <nayanajanrdhan566@gmail.com>
2020-11-12 23:14:20 +0530

    log added

--- sketch.js
+++ sketch.js
@@ -28,6 +28,7 @@ function setup(){
     pig3 = new Pig(810, 220);
 
     log3 =  new Log(810,180,300, PI/2);
+    log6= new Log(500,400,300,PI/2)
 
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
@@ -57,6 +58,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display()
 
     bird.display();
 }
